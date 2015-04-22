41ab80ead2a2d49f72441d140bc93da4c59165e4
WoodZhan <WoodZhan@truelight.com.tw>
2015-04-22 10:11:01 +0800

    [Aardvark] * Add Aardvark i2c adapter function test GUI.

---
+++ hardware/usb/aardvark/FunctionTestPanel.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace aardvark
+{
+    partial class FunctionTestPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lDevAddr = new System.Windows.Forms.Label();
+            this.tbDevAddr = new System.Windows.Forms.TextBox();
+            this.lRegAddr = new System.Windows.Forms.Label();
+            this.tbRegAddr = new System.Windows.Forms.TextBox();
+            this.cbConnectState = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpSignal = new System.Windows.Forms.TabPage();
+            this.bSignalWrite = new System.Windows.Forms.Button();
+            this.bSignalRead = new System.Windows.Forms.Button();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.lValue = new System.Windows.Forms.Label();
+            this.tpMulti = new System.Windows.Forms.TabPage();
+            this.dgvMultiRegister = new System.Windows.Forms.DataGridView();
+            this.bWrite = new System.Windows.Forms.Button();
+            this.bMultiRead = new System.Windows.Forms.Button();
+            this.tbLength = new System.Windows.Forms.TextBox();
+            this.lLength = new System.Windows.Forms.Label();
+            this.lPort = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.lBitrate = new System.Windows.Forms.Label();
+            this.tbBitrate = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tpSignal.SuspendLayout();
+            this.tpMulti.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMultiRegister)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lDevAddr
+            // 
+            this.lDevAddr.AutoSize = true;
+            this.lDevAddr.Location = new System.Drawing.Point(10, 40);
+            this.lDevAddr.Name = "lDevAddr";
+            this.lDevAddr.Size = new System.Drawing.Size(57, 12);
+            this.lDevAddr.TabIndex = 0;
+            this.lDevAddr.Text = "Dev Addr :";
+            // 
+            // tbDevAddr
+            // 
+            this.tbDevAddr.Location = new System.Drawing.Point(73, 37);
+            this.tbDevAddr.Name = "tbDevAddr";
+            this.tbDevAddr.Size = new System.Drawing.Size(30, 22);
+            this.tbDevAddr.TabIndex = 1;
+            this.tbDevAddr.Text = "80";
+            // 
+            // lRegAddr
+            // 
+            this.lRegAddr.AutoSize = true;
+            this.lRegAddr.Location = new System.Drawing.Point(109, 40);
+            this.lRegAddr.Name = "lRegAddr";
+            this.lRegAddr.Size = new System.Drawing.Size(57, 12);
+            this.lRegAddr.TabIndex = 2;
+            this.lRegAddr.Text = "Reg Addr :";
+            // 
+            // tbRegAddr
+            // 
+            this.tbRegAddr.Location = new System.Drawing.Point(172, 37);
+            this.tbRegAddr.Name = "tbRegAddr";
+            this.tbRegAddr.Size = new System.Drawing.Size(30, 22);
+            this.tbRegAddr.TabIndex = 3;
+            this.tbRegAddr.Text = "0";
+            // 
+            // cbConnectState
+            // 
+            this.cbConnectState.AutoSize = true;
+            this.cbConnectState.Location = new System.Drawing.Point(166, 9);
+            this.cbConnectState.Name = "cbConnectState";
+            this.cbConnectState.Size = new System.Drawing.Size(74, 16);
+            this.cbConnectState.TabIndex = 4;
+            this.cbConnectState.Text = "Connected";
+            this.cbConnectState.UseVisualStyleBackColor = true;
+            this.cbConnectState.CheckedChanged += new System.EventHandler(this._cbConnectStateCheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpSignal);
+            this.tabControl1.Controls.Add(this.tpMulti);
+            this.tabControl1.Location = new System.Drawing.Point(3, 65);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(237, 215);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tpSignal
+            // 
+            this.tpSignal.Controls.Add(this.bSignalWrite);
+            this.tpSignal.Controls.Add(this.bSignalRead);
+            this.tpSignal.Controls.Add(this.tbValue);
+            this.tpSignal.Controls.Add(this.lValue);
+            this.tpSignal.Location = new System.Drawing.Point(4, 22);
+            this.tpSignal.Name = "tpSignal";
+            this.tpSignal.Padding = new System.Windows.Forms.Padding(3);
+            this.tpSignal.Size = new System.Drawing.Size(229, 189);
+            this.tpSignal.TabIndex = 0;
+            this.tpSignal.Text = "Signal";
+            this.tpSignal.UseVisualStyleBackColor = true;
+            // 
+            // bSignalWrite
+            // 
+            this.bSignalWrite.Location = new System.Drawing.Point(161, 6);
+            this.bSignalWrite.Name = "bSignalWrite";
+            this.bSignalWrite.Size = new System.Drawing.Size(65, 23);
+            this.bSignalWrite.TabIndex = 3;
+            this.bSignalWrite.Text = "Write";
+            this.bSignalWrite.UseVisualStyleBackColor = true;
+            this.bSignalWrite.Click += new System.EventHandler(this.bSignalWrite_Click);
+            // 
+            // bSignalRead
+            // 
+            this.bSignalRead.Location = new System.Drawing.Point(90, 6);
+            this.bSignalRead.Name = "bSignalRead";
+            this.bSignalRead.Size = new System.Drawing.Size(65, 23);
+            this.bSignalRead.TabIndex = 2;
+            this.bSignalRead.Text = "Read";
+            this.bSignalRead.UseVisualStyleBackColor = true;
+            this.bSignalRead.Click += new System.EventHandler(this.bSignalRead_Click);
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(54, 6);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(30, 22);
+            this.tbValue.TabIndex = 1;
+            // 
+            // lValue
+            // 
+            this.lValue.AutoSize = true;
+            this.lValue.Location = new System.Drawing.Point(10, 10);
+            this.lValue.Name = "lValue";
+            this.lValue.Size = new System.Drawing.Size(38, 12);
+            this.lValue.TabIndex = 0;
+            this.lValue.Text = "Value :";
+            // 
+            // tpMulti
+            // 
+            this.tpMulti.Controls.Add(this.dgvMultiRegister);
+            this.tpMulti.Controls.Add(this.bWrite);
+            this.tpMulti.Controls.Add(this.bMultiRead);
+            this.tpMulti.Controls.Add(this.tbLength);
+            this.tpMulti.Controls.Add(this.lLength);
+            this.tpMulti.Location = new System.Drawing.Point(4, 22);
+            this.tpMulti.Name = "tpMulti";
+            this.tpMulti.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMulti.Size = new System.Drawing.Size(229, 189);
+            this.tpMulti.TabIndex = 1;
+            this.tpMulti.Text = "Multi";
+            this.tpMulti.UseVisualStyleBackColor = true;
+            // 
+            // dgvMultiRegister
+            // 
+            this.dgvMultiRegister.AllowUserToAddRows = false;
+            this.dgvMultiRegister.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvMultiRegister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvMultiRegister.Location = new System.Drawing.Point(83, 3);
+            this.dgvMultiRegister.Name = "dgvMultiRegister";
+            this.dgvMultiRegister.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dgvMultiRegister.RowTemplate.Height = 24;
+            this.dgvMultiRegister.Size = new System.Drawing.Size(143, 183);
+            this.dgvMultiRegister.TabIndex = 4;
+            // 
+            // bWrite
+            // 
+            this.bWrite.Location = new System.Drawing.Point(12, 64);
+            this.bWrite.Name = "bWrite";
+            this.bWrite.Size = new System.Drawing.Size(65, 23);
+            this.bWrite.TabIndex = 3;
+            this.bWrite.Text = "Write";
+            this.bWrite.UseVisualStyleBackColor = true;
+            this.bWrite.Click += new System.EventHandler(this.bWrite_Click);
+            // 
+            // bMultiRead
+            // 
+            this.bMultiRead.Location = new System.Drawing.Point(12, 35);
+            this.bMultiRead.Name = "bMultiRead";
+            this.bMultiRead.Size = new System.Drawing.Size(65, 23);
+            this.bMultiRead.TabIndex = 2;
+            this.bMultiRead.Text = "Read";
+            this.bMultiRead.UseVisualStyleBackColor = true;
+            this.bMultiRead.Click += new System.EventHandler(this._bMultiReadClick);
+            // 
+            // tbLength
+            // 
+            this.tbLength.Location = new System.Drawing.Point(53, 7);
+            this.tbLength.Name = "tbLength";
+            this.tbLength.Size = new System.Drawing.Size(30, 22);
+            this.tbLength.TabIndex = 1;
+            this.tbLength.Text = "6";
+            // 
+            // lLength
+            // 
+            this.lLength.AutoSize = true;
+            this.lLength.Location = new System.Drawing.Point(10, 10);
+            this.lLength.Name = "lLength";
+            this.lLength.Size = new System.Drawing.Size(44, 12);
+            this.lLength.TabIndex = 0;
+            this.lLength.Text = "Length :";
+            // 
+            // lPort
+            // 
+            this.lPort.AutoSize = true;
+            this.lPort.Location = new System.Drawing.Point(10, 10);
+            this.lPort.Name = "lPort";
+            this.lPort.Size = new System.Drawing.Size(30, 12);
+            this.lPort.TabIndex = 6;
+            this.lPort.Text = "Port :";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(46, 7);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(30, 22);
+            this.tbPort.TabIndex = 7;
+            this.tbPort.Text = "0";
+            // 
+            // lBitrate
+            // 
+            this.lBitrate.AutoSize = true;
+            this.lBitrate.Location = new System.Drawing.Point(82, 10);
+            this.lBitrate.Name = "lBitrate";
+            this.lBitrate.Size = new System.Drawing.Size(42, 12);
+            this.lBitrate.TabIndex = 8;
+            this.lBitrate.Text = "Bitrate :";
+            // 
+            // tbBitrate
+            // 
+            this.tbBitrate.Location = new System.Drawing.Point(130, 7);
+            this.tbBitrate.Name = "tbBitrate";
+            this.tbBitrate.Size = new System.Drawing.Size(30, 22);
+            this.tbBitrate.TabIndex = 9;
+            this.tbBitrate.Text = "100";
+            // 
+            // FunctionTestPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbBitrate);
+            this.Controls.Add(this.lBitrate);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.lPort);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.cbConnectState);
+            this.Controls.Add(this.tbRegAddr);
+            this.Controls.Add(this.lRegAddr);
+            this.Controls.Add(this.tbDevAddr);
+            this.Controls.Add(this.lDevAddr);
+            this.MaximumSize = new System.Drawing.Size(243, 283);
+            this.MinimumSize = new System.Drawing.Size(243, 283);
+            this.Name = "FunctionTestPanel";
+            this.Size = new System.Drawing.Size(243, 283);
+            this.tabControl1.ResumeLayout(false);
+            this.tpSignal.ResumeLayout(false);
+            this.tpSignal.PerformLayout();
+            this.tpMulti.ResumeLayout(false);
+            this.tpMulti.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMultiRegister)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lDevAddr;
+        private System.Windows.Forms.TextBox tbDevAddr;
+        private System.Windows.Forms.Label lRegAddr;
+        private System.Windows.Forms.TextBox tbRegAddr;
+        private System.Windows.Forms.CheckBox cbConnectState;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpSignal;
+        private System.Windows.Forms.Button bSignalRead;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.Label lValue;
+        private System.Windows.Forms.TabPage tpMulti;
+        private System.Windows.Forms.Button bSignalWrite;
+        private System.Windows.Forms.Label lPort;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label lBitrate;
+        private System.Windows.Forms.TextBox tbBitrate;
+        private System.Windows.Forms.Button bMultiRead;
+        private System.Windows.Forms.TextBox tbLength;
+        private System.Windows.Forms.Label lLength;
+        private System.Windows.Forms.DataGridView dgvMultiRegister;
+        private System.Windows.Forms.Button bWrite;
+    }
+}
---
+++ hardware/usb/aardvark/FunctionTestPanel.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aardvark
+{
+    public partial class FunctionTestPanel : UserControl
+    {
+        private int iHandle = 0;
+        private DataTable dtValue = new DataTable();
+
+        public FunctionTestPanel()
+        {
+            InitializeComponent();
+            dtValue.Columns.Add("Addr", typeof(Byte));
+            dtValue.Columns.Add("Value", typeof(Byte));
+            dgvMultiRegister.DataSource = dtValue;
+            dgvMultiRegister.Columns["Addr"].ReadOnly = true;
+        }
+
+        private int _DisconnectDevice()
+        {
+            if (iHandle == 0)
+                return 0;
+
+            AardvarkApi.aa_close(iHandle);
+
+            iHandle = 0;
+            tbPort.ReadOnly = false;
+            tbBitrate.ReadOnly = false;
+            cbConnectState.Checked = false;
+
+            return 0;
+        }
+
+        ~FunctionTestPanel()
+        {
+            if (_DisconnectDevice() < 0)
+                MessageBox.Show("_DisconnectDevice() fail!!");
+        }
+
+        private int _ConnectDevice()
+        {
+            int port, bitrate;
+
+            if (iHandle > 0)
+                return 1;
+
+            port = Convert.ToInt32(tbPort.Text);
+            if ((port <= 0) && (port > 10)) {
+                MessageBox.Show("Port out of range (1 ~ 10)!!");
+                goto Error;
+            }
+
+            bitrate = Convert.ToInt32(tbBitrate.Text);
+            if ((bitrate < 10) || (bitrate > 400)) {
+                MessageBox.Show("Bitrate out of range (10 ~ 400)!!");
+                goto Error;
+            }
+
+            // Open the device
+            iHandle = AardvarkApi.aa_open(port);
+            if (iHandle <= 0) {
+                MessageBox.Show("Please check I2C adapter connect!!");
+                goto Error;
+            }
+
+            // Ensure that the I2C subsystem is enabled
+            AardvarkApi.aa_configure(iHandle, AardvarkConfig.AA_CONFIG_SPI_I2C);
+
+            // Enable the I2C bus pullup resistors (2.2k resistors).
+            // This command is only effective on v2.0 hardware or greater.
+            // The pullup resistors on the v1.02 hardware are enabled by default.
+            AardvarkApi.aa_i2c_pullup(iHandle, AardvarkApi.AA_I2C_PULLUP_BOTH);
+
+            // Set the bitrate
+            bitrate = AardvarkApi.aa_i2c_bitrate(iHandle, bitrate);
+
+            tbPort.ReadOnly = true;
+            tbBitrate.ReadOnly = true;
+            cbConnectState.Checked = true;
+            
+            return 0;
+
+        Error:
+            if (_DisconnectDevice() < 0)
+                MessageBox.Show("_DisconnectDevice() fail!!");
+            
+            return -1;
+
+        }
+
+        private void _cbConnectStateCheckedChanged(object sender, EventArgs e)
+        {
+            if (cbConnectState.Checked == true) {
+                if (_ConnectDevice() < 0) {
+                    MessageBox.Show("_ConnectDevice() fail!!");
+                    return;
+                }
+            }
+            else {
+                if (_DisconnectDevice() < 0) {
+                    MessageBox.Show("_DisconnectDevice() fail!!");
+                    return;
+                }
+            }
+        }
+
+        private int _CheckSignalReadInput()
+        {
+            int devAddr, regAddr;
+
+            if (tbDevAddr.Text.Length == 0) {
+                MessageBox.Show("Please input DevAddr!!");
+                return -1;
+            }
+
+            devAddr = Convert.ToInt32(tbDevAddr.Text);
+            if ((devAddr < 0) || (devAddr > 127)) {
+                MessageBox.Show("devAddr out of range (0 ~ 127)!!");
+                return -1;
+            }
+
+            if (tbRegAddr.Text.Length == 0) {
+                MessageBox.Show("Please input RegAddr!!");
+                return -1;
+            }
+
+            regAddr = Convert.ToInt32(tbRegAddr.Text);
+            if ((regAddr < 0) || (regAddr > 255)) {
+                MessageBox.Show("regAddr out of range (0 ~ 255)!!");
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private void bSignalRead_Click(object sender, EventArgs e)
+        {
+            byte[] regAddr = new byte[1];
+            byte[] data = new byte[1];
+            int counter;
+            byte devAddr;
+
+            if (_ConnectDevice() < 0)
+                return;
+
+            if (_CheckSignalReadInput() < 0)
+                return;
+
+            devAddr = Convert.ToByte(Convert.ToInt32(tbDevAddr.Text));
+            regAddr[0] = Convert.ToByte(Convert.ToInt32(tbRegAddr.Text));
+
+            AardvarkApi.aa_i2c_write(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_STOP, 1, regAddr);
+
+            counter = AardvarkApi.aa_i2c_read(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_FLAGS, 1, data);
+            if (counter < 0) {
+                MessageBox.Show("AardvarkApi.aa_i2c_read() fail: " + counter);
+                return;
+            }
+            else if (counter == 0) {
+                MessageBox.Show("Read 0 byte.\n" +
+                    "Please check devAddr!!");
+                return;
+            }
+
+            tbValue.Text = Convert.ToString(data[0]);
+        }
+
+        private int _CheckSignalWriteInput()
+        {
+            int data;
+
+            if (_CheckSignalReadInput() < 0)
+                return -1;        
+
+            if (tbDevAddr.Text.Length == 0) {
+                MessageBox.Show("Please input DevAddr!!");
+                return -1;
+            }
+
+            data = Convert.ToInt32(tbValue.Text);
+            if ((data < 0) || (data > 255)) {
+                MessageBox.Show("Value out of range (0 ~ 255)!!");
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private void bSignalWrite_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[2];
+            byte devAddr;
+
+            if (_ConnectDevice() < 0)
+                return;
+
+            if (_CheckSignalWriteInput() < 0)
+                return;
+
+            devAddr = Convert.ToByte(Convert.ToInt32(tbDevAddr.Text));
+            data[0] = Convert.ToByte(Convert.ToInt32(tbRegAddr.Text));
+            data[1] = Convert.ToByte(Convert.ToInt32(tbValue.Text));
+
+            AardvarkApi.aa_i2c_write(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_FLAGS, 2, data);
+            AardvarkApi.aa_sleep_ms(10);
+        }
+
+        private int _CheckMultiReadInput()
+        {
+            int regAddr, length;
+
+            if (_CheckSignalReadInput() < 0)
+                return -1;
+
+            if (tbLength.Text.Length == 0) {
+                MessageBox.Show("Please input length!!");
+                return -1;
+            }
+            regAddr = Convert.ToInt32(tbRegAddr.Text);
+            length = Convert.ToInt32(tbLength.Text);
+            if ((length + regAddr) > 255) {
+                MessageBox.Show("Read out of range (0 ~ 255)!!");
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private void _bMultiReadClick(object sender, EventArgs e)
+        {
+            byte[] buf = new byte[256];
+            int counter;
+            byte[] regAddr = new byte[1];
+            byte devAddr, length;
+
+            dtValue.Clear();
+
+            if (_ConnectDevice() < 0)
+                return;
+
+            if (_CheckMultiReadInput() < 0)
+                return;
+            devAddr = Convert.ToByte(Convert.ToInt32(tbDevAddr.Text));
+            regAddr[0] = Convert.ToByte(Convert.ToInt32(tbRegAddr.Text));
+            length = Convert.ToByte(Convert.ToInt32(tbLength.Text));
+
+            AardvarkApi.aa_i2c_write(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_STOP, 1, regAddr);
+
+            counter = AardvarkApi.aa_i2c_read(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_FLAGS, length, buf);
+            if (counter < 0) {
+                MessageBox.Show("AardvarkApi.aa_i2c_read() fail: " + counter);
+                return;
+            }
+            else if (counter == 0) {
+                MessageBox.Show("Read 0 byte.\n" +
+                    "Please check devAddr!!");
+                return;
+            }
+
+            for (int i = 0; i < length; i++)
+                dtValue.Rows.Add(regAddr[0] + i, buf[i]);
+        }
+
+        private void bWrite_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[256];
+            int index;
+            byte devAddr, length;
+
+            if (_ConnectDevice() < 0)
+                return;
+
+            if (_CheckMultiReadInput() < 0)
+                return;
+
+            devAddr = Convert.ToByte(Convert.ToInt32(tbDevAddr.Text));
+            length = Convert.ToByte(Convert.ToInt32(tbLength.Text) + 1);
+            
+            foreach (DataRow row in dtValue.Rows) {
+                index = dtValue.Rows.IndexOf(row);
+                if (index == 0)
+                    data[0] = Convert.ToByte(row.ItemArray[0]);
+                data[index + 1] = Convert.ToByte(row.ItemArray[1]);
+            }
+            AardvarkApi.aa_i2c_write(iHandle, devAddr, AardvarkI2cFlags.AA_I2C_NO_FLAGS, length, data);
+            AardvarkApi.aa_sleep_ms(10);
+        }
+
+
+    }
+}
