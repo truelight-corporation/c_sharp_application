c0fc1bc8b3d4a933f12890963b35522f685a21ef
WoodZhan <WoodZhan@truelight.com.tw>
2015-06-13 12:03:00 +0800

    [Hxt6104aHxr6104aConfig] * Modify GUI size.

--- project/Hxt6104aHxr6104aConfig/FHxt6104aHxr6104aConfig.Designer.cs
+++ project/Hxt6104aHxr6104aConfig/FHxt6104aHxr6104aConfig.Designer.cs
@@ -63,7 +63,7 @@
             this.tcHxt6104aHxr6104aConfig.Location = new System.Drawing.Point(12, 34);
             this.tcHxt6104aHxr6104aConfig.Name = "tcHxt6104aHxr6104aConfig";
             this.tcHxt6104aHxr6104aConfig.SelectedIndex = 0;
-            this.tcHxt6104aHxr6104aConfig.Size = new System.Drawing.Size(583, 627);
+            this.tcHxt6104aHxr6104aConfig.Size = new System.Drawing.Size(583, 615);
             this.tcHxt6104aHxr6104aConfig.TabIndex = 2;
             // 
             // tpHxr6104aConfig
@@ -72,7 +72,7 @@
             this.tpHxr6104aConfig.Location = new System.Drawing.Point(4, 22);
             this.tpHxr6104aConfig.Name = "tpHxr6104aConfig";
             this.tpHxr6104aConfig.Padding = new System.Windows.Forms.Padding(3);
-            this.tpHxr6104aConfig.Size = new System.Drawing.Size(575, 601);
+            this.tpHxr6104aConfig.Size = new System.Drawing.Size(575, 594);
             this.tpHxr6104aConfig.TabIndex = 0;
             this.tpHxr6104aConfig.Text = "HXR6104A";
             this.tpHxr6104aConfig.UseVisualStyleBackColor = true;
@@ -83,7 +83,7 @@
             this.tpHxt6104aConfig.Location = new System.Drawing.Point(4, 22);
             this.tpHxt6104aConfig.Name = "tpHxt6104aConfig";
             this.tpHxt6104aConfig.Padding = new System.Windows.Forms.Padding(3);
-            this.tpHxt6104aConfig.Size = new System.Drawing.Size(575, 601);
+            this.tpHxt6104aConfig.Size = new System.Drawing.Size(575, 589);
             this.tpHxt6104aConfig.TabIndex = 1;
             this.tpHxt6104aConfig.Text = "HXT6104A";
             this.tpHxt6104aConfig.UseVisualStyleBackColor = true;
@@ -92,14 +92,14 @@
             // 
             this.ucHxt6104aConfig.Location = new System.Drawing.Point(6, 6);
             this.ucHxt6104aConfig.Name = "ucHxt6104aConfig";
-            this.ucHxt6104aConfig.Size = new System.Drawing.Size(563, 592);
+            this.ucHxt6104aConfig.Size = new System.Drawing.Size(563, 582);
             this.ucHxt6104aConfig.TabIndex = 0;
             // 
             // FHxt6104aHxr6104aConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(606, 664);
+            this.ClientSize = new System.Drawing.Size(606, 655);
             this.Controls.Add(this.tcHxt6104aHxr6104aConfig);
             this.Controls.Add(this.cbConnected);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
