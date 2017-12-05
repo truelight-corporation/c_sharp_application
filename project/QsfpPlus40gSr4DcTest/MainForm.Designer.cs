c637f7fe0a36c7272205483ae79562365b1ccbe2
WoodZhan <WoodZhan@truelight.com.tw>
2017-12-05 11:35:27 +0800

    [QsfpPlus40gSr4DcTest] * Add QSFP+ 40G SR4 DC test GUI source code.

---
+++ project/QsfpPlus40gSr4DcTest/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QsfpPlus40gSr4DcTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbI2cAdapterConnected = new System.Windows.Forms.CheckBox();
+            this.cbPowerMeterConnected = new System.Windows.Forms.CheckBox();
+            this.tbPowerMeterIpAddr = new System.Windows.Forms.TextBox();
+            this.lPowerMeterIpAddr = new System.Windows.Forms.Label();
+            this.cbAutoMonitor = new System.Windows.Forms.CheckBox();
+            this.ucQsfpPlus40gSr4DcTest = new QsfpPlus40gSr4DcTest.UcQsfpPlus40gSr4DcTest();
+            this.SuspendLayout();
+            // 
+            // cbI2cAdapterConnected
+            // 
+            this.cbI2cAdapterConnected.AutoSize = true;
+            this.cbI2cAdapterConnected.Location = new System.Drawing.Point(366, 13);
+            this.cbI2cAdapterConnected.Name = "cbI2cAdapterConnected";
+            this.cbI2cAdapterConnected.Size = new System.Drawing.Size(114, 16);
+            this.cbI2cAdapterConnected.TabIndex = 0;
+            this.cbI2cAdapterConnected.Text = "I2C轉接器已連結";
+            this.cbI2cAdapterConnected.UseVisualStyleBackColor = true;
+            this.cbI2cAdapterConnected.CheckedChanged += new System.EventHandler(this.cbI2cAdapterConnected_CheckedChanged);
+            // 
+            // cbPowerMeterConnected
+            // 
+            this.cbPowerMeterConnected.AutoSize = true;
+            this.cbPowerMeterConnected.BackColor = System.Drawing.SystemColors.Control;
+            this.cbPowerMeterConnected.Location = new System.Drawing.Point(12, 12);
+            this.cbPowerMeterConnected.Name = "cbPowerMeterConnected";
+            this.cbPowerMeterConnected.Size = new System.Drawing.Size(132, 16);
+            this.cbPowerMeterConnected.TabIndex = 1;
+            this.cbPowerMeterConnected.Text = "光強度量測儀已連結";
+            this.cbPowerMeterConnected.UseVisualStyleBackColor = false;
+            this.cbPowerMeterConnected.CheckedChanged += new System.EventHandler(this.cbPowerMeterConnected_CheckedChanged);
+            // 
+            // tbPowerMeterIpAddr
+            // 
+            this.tbPowerMeterIpAddr.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.tbPowerMeterIpAddr.Location = new System.Drawing.Point(260, 10);
+            this.tbPowerMeterIpAddr.Name = "tbPowerMeterIpAddr";
+            this.tbPowerMeterIpAddr.Size = new System.Drawing.Size(100, 22);
+            this.tbPowerMeterIpAddr.TabIndex = 3;
+            this.tbPowerMeterIpAddr.Text = "172.16.102.50";
+            // 
+            // lPowerMeterIpAddr
+            // 
+            this.lPowerMeterIpAddr.AutoSize = true;
+            this.lPowerMeterIpAddr.Location = new System.Drawing.Point(150, 13);
+            this.lPowerMeterIpAddr.Name = "lPowerMeterIpAddr";
+            this.lPowerMeterIpAddr.Size = new System.Drawing.Size(104, 12);
+            this.lPowerMeterIpAddr.TabIndex = 4;
+            this.lPowerMeterIpAddr.Text = "光強度量測儀位址:";
+            // 
+            // cbAutoMonitor
+            // 
+            this.cbAutoMonitor.AutoSize = true;
+            this.cbAutoMonitor.BackColor = System.Drawing.SystemColors.Control;
+            this.cbAutoMonitor.Enabled = false;
+            this.cbAutoMonitor.Location = new System.Drawing.Point(1213, 13);
+            this.cbAutoMonitor.Name = "cbAutoMonitor";
+            this.cbAutoMonitor.Size = new System.Drawing.Size(72, 16);
+            this.cbAutoMonitor.TabIndex = 5;
+            this.cbAutoMonitor.Text = "開始量測";
+            this.cbAutoMonitor.UseVisualStyleBackColor = false;
+            this.cbAutoMonitor.CheckedChanged += new System.EventHandler(this.cbAutoMonitor_CheckedChanged);
+            // 
+            // ucQsfpPlus40gSr4DcTest
+            // 
+            this.ucQsfpPlus40gSr4DcTest.Location = new System.Drawing.Point(12, 38);
+            this.ucQsfpPlus40gSr4DcTest.Name = "ucQsfpPlus40gSr4DcTest";
+            this.ucQsfpPlus40gSr4DcTest.Size = new System.Drawing.Size(1280, 698);
+            this.ucQsfpPlus40gSr4DcTest.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 741);
+            this.Controls.Add(this.cbAutoMonitor);
+            this.Controls.Add(this.cbI2cAdapterConnected);
+            this.Controls.Add(this.cbPowerMeterConnected);
+            this.Controls.Add(this.tbPowerMeterIpAddr);
+            this.Controls.Add(this.lPowerMeterIpAddr);
+            this.Controls.Add(this.ucQsfpPlus40gSr4DcTest);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1305, 768);
+            this.MinimumSize = new System.Drawing.Size(1305, 766);
+            this.Name = "MainForm";
+            this.Text = "QSFP+ 40G SR4 DC Test 20171201";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbI2cAdapterConnected;
+        private System.Windows.Forms.CheckBox cbPowerMeterConnected;
+        private UcQsfpPlus40gSr4DcTest ucQsfpPlus40gSr4DcTest;
+        private System.Windows.Forms.TextBox tbPowerMeterIpAddr;
+        private System.Windows.Forms.Label lPowerMeterIpAddr;
+        private System.Windows.Forms.CheckBox cbAutoMonitor;
+    }
+}
---
+++ project/QsfpPlus40gSr4DcTest/MainForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using I2cMasterInterface;
+using ExfoIqs1600ScpiInterface;
+
+namespace QsfpPlus40gSr4DcTest
+{
+    public partial class MainForm : Form
+    {
+        private I2cMaster i2cAdapter = new I2cMaster();
+        private ExfoIqs1600Scpi powerMeter = new ExfoIqs1600Scpi();
+
+        private int _I2cConnect()
+        {
+            if (i2cAdapter.ConnectApi(100) < 0)    
+                return -1;
+
+            if (i2cAdapter.connected == true) {
+                i2cAdapter.SetTimeoutApi(50);
+                cbI2cAdapterConnected.Checked = true;
+            }
+            else
+                cbI2cAdapterConnected.Checked = false;
+
+            return 0;
+        }
+
+        private int _I2cDisconnect()
+        {
+            if (i2cAdapter.DisconnectApi() < 0)
+                return -1;
+
+            cbI2cAdapterConnected.Checked = false;
+
+            return 0;
+        }
+
+        private int _I2cRead(byte devAddr, byte regAddr, byte length, byte[] data)
+        {
+            int rv;
+
+            if (i2cAdapter.connected == false) {
+                if (_I2cConnect() < 0)
+                    return -1;
+            }
+
+            rv = i2cAdapter.ReadApi(devAddr, regAddr, length, data);
+            if (rv < 0) {
+                MessageBox.Show("QSFP+ module no response!!");
+                return -1;
+            }
+            
+            return rv;
+        }
+
+        private int _I2cWrite(byte devAddr, byte regAddr, byte length, byte[] data)
+        {
+            int rv;
+
+            if (i2cAdapter.connected == false) {
+                if (_I2cConnect() < 0)
+                    return -1;
+            }
+
+            rv = i2cAdapter.WriteApi(devAddr, regAddr, length, data);
+            if (rv < 0) {
+                MessageBox.Show("QSFP+ module no response!!");
+                return -1;
+            }
+            
+            return rv;
+        }
+
+        private int _PowerMeterConnect()
+        {
+            tbPowerMeterIpAddr.Enabled = false;
+
+            if (tbPowerMeterIpAddr.Text.Length < 7) {
+                MessageBox.Show("Please input Power meter device IP address!!");
+                tbPowerMeterIpAddr.Enabled = true;
+                return -1;
+            }
+
+            if (powerMeter.ConnectApi(tbPowerMeterIpAddr.Text) < 0) {
+                tbPowerMeterIpAddr.Enabled = true;
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private int _PowerMeterRead(string[] buffer)
+        {
+            if (cbPowerMeterConnected.Checked == false) {
+                buffer[0] = buffer[1] = buffer[2] = buffer[3] = "NA";
+                return -1;
+            }
+
+            if (powerMeter.ReadPower(buffer) < 0)
+                return -1;
+
+            return 0;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            if (ucQsfpPlus40gSr4DcTest.SetI2cReadCBApi(_I2cRead) < 0) {
+                MessageBox.Show("ucQsfpPlus40gSr4DcTest.SetI2cReadCBApi() faile Error!!");
+                return;
+            }
+            if (ucQsfpPlus40gSr4DcTest.SetI2cWriteCBApi(_I2cWrite) < 0) {
+                MessageBox.Show("ucQsfpPlus40gSr4DcTest.SetI2cWriteCBApi() faile Error!!");
+                return;
+            }
+            if (ucQsfpPlus40gSr4DcTest.SetPowerMeterReadCBApi(_PowerMeterRead) < 0) {
+                MessageBox.Show("ucQsfpPlus40gSr4DcTest.SetPowerMeterReadCBApi() faile Error!!");
+                return;
+            }
+        }
+
+        private void cbI2cAdapterConnected_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbI2cAdapterConnected.Checked == true) {
+                _I2cConnect();
+                cbAutoMonitor.Enabled = true;
+                if (cbPowerMeterConnected.Checked == true)
+                    cbAutoMonitor.Checked = true;
+            }
+            else {
+                if (cbPowerMeterConnected.Checked == false) {
+                    ucQsfpPlus40gSr4DcTest.StopMonitorApi();
+                    cbAutoMonitor.Checked = false;
+                    cbAutoMonitor.Enabled = false;
+                }
+                _I2cDisconnect();
+            }
+        }
+
+        private void cbPowerMeterConnected_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbPowerMeterConnected.Checked == true) {
+                if (_PowerMeterConnect() < 0) {
+                    cbPowerMeterConnected.Checked = false;
+                    return;
+                }
+                cbPowerMeterConnected.Enabled = false;
+                cbAutoMonitor.Enabled = true;
+            }
+            else {
+
+                cbPowerMeterConnected.Checked = false;
+                if (cbAutoMonitor.Checked == true) {
+                    
+                    ucQsfpPlus40gSr4DcTest.StopMonitorApi();
+                }
+                if (cbI2cAdapterConnected.Checked == false)
+                    cbAutoMonitor.Enabled = false;
+                tbPowerMeterIpAddr.Enabled = true;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ucQsfpPlus40gSr4DcTest.StopMonitorApi();
+            ucQsfpPlus40gSr4DcTest.SaveFileCheckApi(sender, e);
+        }
+
+        private void cbAutoMonitor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbAutoMonitor.Checked == true)
+                ucQsfpPlus40gSr4DcTest.StartMonitorApi();
+            else
+                ucQsfpPlus40gSr4DcTest.StopMonitorApi();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString()) {
+                case "Space":
+                    ucQsfpPlus40gSr4DcTest.SetFocusOnLogFilePathApi();
+                    ucQsfpPlus40gSr4DcTest.SpaceKeyDownApi(sender, e);
+                    break;
+                
+                case "Right":
+                    ucQsfpPlus40gSr4DcTest.RightKeyDownApi(sender, e);
+                    break;
+
+                case "Left":
+                    ucQsfpPlus40gSr4DcTest.LeftKeyDownApi(sender, e);
+                    break;
+
+                default:
+                    break;
+            }
+            
+        }
+
+
+    }
+}
