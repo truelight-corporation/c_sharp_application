320c871167272c7ce7195bb3fdd7a110998a3e69
WoodZhan <WoodZhan@truelight.com.tw>
2023-11-10 17:13:43 +0800

    [QsfpDigitalDiagnosticMonitoring] * Add mem dump function.

---
+++ project/QsfpDigitalDiagnosticMonitoring/UCMemoryDump.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace QsfpDigitalDiagnosticMonitoring
+{
+    partial class UCMemoryDump
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lPage = new System.Windows.Forms.Label();
+            this.cbPageSelect = new System.Windows.Forms.ComboBox();
+            this.dgvMemory = new System.Windows.Forms.DataGridView();
+            this.bRead = new System.Windows.Forms.Button();
+            this.bWrite = new System.Windows.Forms.Button();
+            this.bWriteLowPage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lPage
+            // 
+            this.lPage.AutoSize = true;
+            this.lPage.Location = new System.Drawing.Point(782, 9);
+            this.lPage.Name = "lPage";
+            this.lPage.Size = new System.Drawing.Size(30, 12);
+            this.lPage.TabIndex = 0;
+            this.lPage.Text = "Page:";
+            // 
+            // cbPageSelect
+            // 
+            this.cbPageSelect.FormattingEnabled = true;
+            this.cbPageSelect.Location = new System.Drawing.Point(818, 6);
+            this.cbPageSelect.Name = "cbPageSelect";
+            this.cbPageSelect.Size = new System.Drawing.Size(80, 20);
+            this.cbPageSelect.TabIndex = 1;
+            // 
+            // dgvMemory
+            // 
+            this.dgvMemory.AllowUserToAddRows = false;
+            this.dgvMemory.AllowUserToDeleteRows = false;
+            this.dgvMemory.AllowUserToResizeColumns = false;
+            this.dgvMemory.AllowUserToResizeRows = false;
+            this.dgvMemory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvMemory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMemory.Location = new System.Drawing.Point(5, 3);
+            this.dgvMemory.Name = "dgvMemory";
+            this.dgvMemory.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvMemory.RowTemplate.Height = 24;
+            this.dgvMemory.Size = new System.Drawing.Size(724, 214);
+            this.dgvMemory.TabIndex = 2;
+            // 
+            // bRead
+            // 
+            this.bRead.Location = new System.Drawing.Point(823, 32);
+            this.bRead.Name = "bRead";
+            this.bRead.Size = new System.Drawing.Size(75, 23);
+            this.bRead.TabIndex = 3;
+            this.bRead.Text = "Read";
+            this.bRead.UseVisualStyleBackColor = true;
+            this.bRead.Click += new System.EventHandler(this.bRead_Click);
+            // 
+            // bWrite
+            // 
+            this.bWrite.Location = new System.Drawing.Point(823, 100);
+            this.bWrite.Name = "bWrite";
+            this.bWrite.Size = new System.Drawing.Size(75, 23);
+            this.bWrite.TabIndex = 4;
+            this.bWrite.Text = "Write";
+            this.bWrite.UseVisualStyleBackColor = true;
+            this.bWrite.Click += new System.EventHandler(this.bWrite_Click);
+            // 
+            // bWriteLowPage
+            // 
+            this.bWriteLowPage.Enabled = false;
+            this.bWriteLowPage.Location = new System.Drawing.Point(807, 160);
+            this.bWriteLowPage.Name = "bWriteLowPage";
+            this.bWriteLowPage.Size = new System.Drawing.Size(91, 23);
+            this.bWriteLowPage.TabIndex = 5;
+            this.bWriteLowPage.Text = "Write Low Page";
+            this.bWriteLowPage.UseVisualStyleBackColor = true;
+            this.bWriteLowPage.Click += new System.EventHandler(this.bWriteLowPage_Click);
+            // 
+            // UCMemoryDump
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.bWriteLowPage);
+            this.Controls.Add(this.bWrite);
+            this.Controls.Add(this.bRead);
+            this.Controls.Add(this.dgvMemory);
+            this.Controls.Add(this.cbPageSelect);
+            this.Controls.Add(this.lPage);
+            this.Name = "UCMemoryDump";
+            this.Size = new System.Drawing.Size(901, 708);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lPage;
+        private System.Windows.Forms.ComboBox cbPageSelect;
+        private System.Windows.Forms.DataGridView dgvMemory;
+        private System.Windows.Forms.Button bRead;
+        private System.Windows.Forms.Button bWrite;
+        private System.Windows.Forms.Button bWriteLowPage;
+    }
+}
---
+++ project/QsfpDigitalDiagnosticMonitoring/UCMemoryDump.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QsfpDigitalDiagnosticMonitoring
+{
+    public partial class UCMemoryDump : UserControl
+    {
+        public delegate int I2cReadCB(byte devAddr, byte regAddr, byte length, byte[] data);
+        public delegate int I2cWriteCB(byte devAddr, byte regAddr, byte length, byte[] data);
+        public delegate int WritePasswordCB();
+
+        private I2cReadCB i2cReadCB = null;
+        private I2cWriteCB i2cWriteCB = null;
+        private WritePasswordCB writePasswordCB = null;
+
+        private DataTable dtMemory = new DataTable();
+
+        public UCMemoryDump()
+        {
+            int i;
+
+            InitializeComponent();
+
+            for (i = 0; i < 16; i++)
+                dtMemory.Columns.Add(i.ToString("X2"), typeof(String));
+            
+            for (i = 0; i < 128; i++) {
+                if (i % 16 == 0) {
+                    dtMemory.Rows.Add("NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA");
+                }
+            }
+            dgvMemory.DataSource = dtMemory;
+            for (i = 0; i < dgvMemory.Rows.Count; i++)
+                dgvMemory.Rows[i].HeaderCell.Value = (i * 16).ToString("X2");
+
+            cbPageSelect.Items.Add("Low Page");
+            cbPageSelect.Items.Add("Up 00h");
+            cbPageSelect.Items.Add("Up 01h");
+            cbPageSelect.Items.Add("Up 02h");
+            cbPageSelect.Items.Add("Up 03h");
+            cbPageSelect.Text = "Up 00h";
+        }
+
+        public int SetI2cReadCBApi(I2cReadCB cb)
+        {
+            if (cb == null)
+                return -1;
+
+            i2cReadCB = new I2cReadCB(cb);
+
+            return 0;
+        }
+
+        public int SetI2cWriteCBApi(I2cWriteCB cb)
+        {
+            if (cb == null)
+                return -1;
+
+            i2cWriteCB = new I2cWriteCB(cb);
+
+            return 0;
+        }
+
+        public int SetWritePasswordCBApi(WritePasswordCB cb)
+        {
+            if (cb == null)
+                return -1;
+
+            writePasswordCB = new WritePasswordCB(cb);
+
+            return 0;
+        }
+
+        private int _ChangePage()
+        {
+            byte[] pageData = new byte[1];
+
+            if (i2cWriteCB == null)
+                return -1;
+
+            switch (cbPageSelect.SelectedItem.ToString()) {
+                case "Low Page":
+                    return 0;
+
+                case "Up 00h":
+                    pageData[0] = 0x00;
+                    break;
+
+                case "Up 01h":
+                    pageData[0] = 0x01;
+                    break;
+
+                case "Up 02h":
+                    pageData[0] = 0x02;
+                    break;
+
+                case "Up 03h":
+                    pageData[0] = 0x03;
+                    break;
+
+                default:
+                    return -1;
+            }
+
+            if (i2cWriteCB(80, 127, 1, pageData) < 0)
+                return -1;
+
+            return 0;
+        }
+
+        private void bRead_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[128];
+            int i;
+            byte starAddr;
+
+            if (i2cReadCB == null)
+                return;
+
+            bRead.Enabled = false;
+
+            if (cbPageSelect.SelectedItem.Equals("Low Page"))
+                starAddr = 0;
+            else {
+                if (_ChangePage() < 0)
+                    goto exit;
+                starAddr = 128;
+            }
+
+            if (i2cReadCB(80, starAddr, 128, data) != 128)
+                goto exit;
+
+            for (i = 0; i < 128; i++)
+                dtMemory.Rows[i / 16].SetField(i % 16, data[i].ToString("X2"));
+
+            if (starAddr == 0)
+                bWriteLowPage.Enabled = true;
+            else
+                bWriteLowPage.Enabled = false;
+
+            exit:
+            bRead.Enabled = true;
+        }
+
+        private void bWrite_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[128];
+            int i;
+            byte starAddr;
+
+            if (writePasswordCB == null)
+                return;
+
+            if (i2cWriteCB == null)
+                return;
+
+            bWrite.Enabled = false;
+
+            if (writePasswordCB() < 0)
+                goto exit;
+
+            if (cbPageSelect.SelectedItem.Equals("Low Page"))
+                starAddr = 0;
+            else {
+                if (_ChangePage() < 0)
+                    goto exit;
+                starAddr = 128;
+            }
+
+            for (i = 0; i < 128; i++)
+                data[i] = Convert.ToByte(dtMemory.Rows[i / 16].ItemArray[i % 16].ToString(), 16);
+
+            if (i2cWriteCB(80, starAddr, 128, data) < 0)
+                goto exit;
+
+            exit:
+            bWrite.Enabled = true;
+        }
+
+        private void bWriteLowPage_Click(object sender, EventArgs e)
+        {
+            byte[] data = {0xA9, 0x46, 0x50, 0x54};
+            int i;
+
+            if (i2cWriteCB == null)
+                return;
+
+            bWriteLowPage.Enabled = false;
+
+            if (i2cWriteCB(80, 0x7B, 4, data) < 0)
+                goto exit;
+
+            data = new byte[123];
+
+            for (i = 0; i < 123; i++)
+                data[i] = Convert.ToByte(dtMemory.Rows[i / 16].ItemArray[i % 16].ToString(), 16);
+
+            System.Threading.Thread.Sleep(1000);
+
+            if (i2cWriteCB(80, 0, 123, data) < 0)
+                goto exit;
+
+            data[0] = 0x06;
+            if (i2cWriteCB(80, 127, 1, data) < 0)
+                goto exit;
+
+            data[0] = 0x80;
+            if (i2cWriteCB(80, 0x82, 1, data) < 0)
+                goto exit;
+
+            System.Threading.Thread.Sleep(100);
+
+            MessageBox.Show("Please re-plug QSFP28 module!!");
+
+            exit:
+            bWriteLowPage.Enabled = false;
+        }
+    }
+}
