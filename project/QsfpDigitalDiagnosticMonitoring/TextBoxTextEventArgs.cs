3ff16463b4b964729bcc1c18cc5188d9789765f8
Dominic0829 <dominicyu0829@gmail.com>
2024-11-05 17:32:31 +0800

    [QsfpDigitalDiagnosticMonitoring] * Merge from integrated GUI project.

---
+++ project/QsfpDigitalDiagnosticMonitoring/TextBoxTextEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QsfpDigitalDiagnosticMonitoring
+{
+    public class TextBoxTextEventArgs: EventArgs
+    {
+        public string Text1 { get; private set; }
+        public string Text2 { get; private set; }
+        public string Text3 { get; private set; }
+        public string Text4 { get; private set; }
+
+        public TextBoxTextEventArgs(string text1, string text2, string text3, string text4)
+        {
+            Text1 = text1;
+            Text2 = text2;
+            Text3 = text3;
+            Text4 = text4;
+        }
+    }
+}
